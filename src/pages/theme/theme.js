c5a9ea5ca80e14abcea95d8ddef7a9c7aac47f25
Aniruddh Chennapragada <anic@berkeley.edu>
2021-11-27 22:01:17 -0800

    [BVC-14] Added fonts and theme to chakra UI

---
+++ src/pages/theme/theme.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+});
+
+export default theme;
