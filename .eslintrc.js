87e73fe1e24978a860d8fffdba62b1ae9010228d
Edward Luna <75227309+misterluna@users.noreply.github.com>
2021-11-29 21:55:19 -0800

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,30 @@
-
-
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features-old
-        sourceType: 'module', // Allows for the use of imports
-        ecmaFeatures: {
-            jsx: true, // Allows for the parsing of JSX
-        },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features-old
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
     },
-    settings: {
-        react: {
-            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-        'plugin:import/errors',
-        'plugin:import/warnings',
-    ],
-    rules: {
-        '@typescript-eslint/ban-ts-comment': 0,
-        'react/prop-types': 0,
-        'import/no-unresolved': 0,
-        '@typescript-eslint/no-unused-vars': 2,
-        'prettier/prettier': ['error', { tabWidth: 2 }],
-    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 0,
+    'react/prop-types': 0,
+    'import/no-unresolved': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+    'prettier/prettier': ['error', { tabWidth: 2 }],
+  },
 };
