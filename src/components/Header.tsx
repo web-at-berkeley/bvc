dfcb4286dac368b5fa1f8a4eda79a475aff8c5d0
Aniruddh Chennapragada <anic@Aniruddhs-MBP.modem>
2021-12-28 18:26:01 +1100

    Added Lo-Fi Header with dummy nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,70 @@
 import React from 'react';
-
-import { Box, HStack, Text } from '@chakra-ui/react';
+import {NavLink, Link, Router} from 'react-router-dom'
+import { Box, HStack, Text, Flex, Heading, ChakraProvider } from '@chakra-ui/react';
+import NetlifyCmsApp from 'netlify-cms-app';
+import Fonts from '../fonts'
+import theme from '../theme'
 
 interface HeaderProps {
   pages: string[];
 }
 
+let styles = {
+  logo: {
+    fontSize: '30px',
+    lineHeight: '45px',
+    letterSpacing: '0.1em',
+    color: '#636363',
+  }
+}
+
 export const Header = ({ pages }: HeaderProps) => (
+  <ChakraProvider theme={theme}>
+    <Fonts />
   <Box w="100%" h="70" backgroundColor="#E5E5E5">
-    <HStack>
-      <Text>BVC</Text>
-      {pages[0]}
-    </HStack>
+  <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={5}
+      color="black"
+    >
+      <Flex align="center" mr={5}>
+
+        <Heading style={styles.logo}>
+          BVC
+        </Heading>
+        
+      </Flex>
+
+      <HStack spacing={4}>
+        <NavLink to="/">
+          <h5>{pages[0]}</h5>
+        </NavLink>
+        <NavLink to="/">
+          <Text>{pages[1]}</Text>
+        </NavLink>
+        <NavLink to="/">
+          <Text>{pages[2]}</Text>
+        </NavLink>
+        <NavLink to="/">
+        <Text>{pages[3]}</Text>
+        </NavLink>
+        <NavLink to="/">
+        <Text>{pages[4]}</Text>
+        </NavLink>
+        <NavLink to="/">
+        <Text>{pages[5]}</Text>
+        </NavLink>
+        <NavLink to="/">
+        <Text>{pages[6]}</Text>
+        </NavLink>
+      </HStack>
+        
+
+  </Flex>
   </Box>
+  </ChakraProvider>
+
 );
