743ce9e71c17eb6411a965f138eac1c97699d118
Edward Luna <75227309+misterluna@users.noreply.github.com>
2021-12-30 22:39:19 -0800

    remove CRA from storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,6 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
   ],
   "core": {
     "builder": "webpack5"
