155915613db45eefc2c4c43faa0f295f10546ef1
Rishi Khare <rishi.khare@berkeley.edu>
2022-01-02 20:39:52 -0600

    [BVC-31] Changed imports in footer file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { FaFacebookF } from '@react-icons/all-files/fa/FaFacebookF';
+import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram';
+import { FaLinkedinIn } from '@react-icons/all-files/fa/FaLinkedinIn';
+import { AiOutlineMail } from '@react-icons/all-files/ai/AiOutlineMail';
 
-import { Box, HStack, Text } from '@chakra-ui/react';
+import { Flex, Spacer, Link, HStack, Text, Image } from '@chakra-ui/react';
+import badge from '../assets/images/badge.png';
 
 interface FooterProps {
   facebook: string;
@@ -9,13 +14,32 @@ interface FooterProps {
   email: string;
 }
 
+const iconStyles = {
+  color: 'white',
+  'margin-right': '20px',
+};
+
 export const Footer = ({ facebook, instagram, linkedin, email }: FooterProps) => (
-  <Box w="100%" h="70" backgroundColor="#464646">
-    <HStack>
-      <Text>{facebook}</Text>
-      <Text>{instagram}</Text>
-      <Text>{linkedin}</Text>
-      <Text>{email}</Text>
+  <Flex w="100%" h="70" backgroundColor="#464646" align="center">
+    <HStack marginLeft="40px">
+      <Link href={facebook}>
+        <FaFacebookF style={iconStyles} />
+      </Link>
+      <Link href={instagram}>
+        <FaInstagram style={iconStyles} />
+      </Link>
+      <Link href={linkedin}>
+        <FaLinkedinIn style={iconStyles} />
+      </Link>
+      <Link href={email}>
+        <AiOutlineMail margin-right="0px" style={iconStyles} />
+      </Link>
     </HStack>
-  </Box>
+    <Spacer />
+    <Text font="Poppins" textStyle="h6" color="white">
+      ©2022 BVC
+    </Text>
+    <Spacer />
+    <Image height={8} src={badge} marginRight="40px" />
+  </Flex>
 );
