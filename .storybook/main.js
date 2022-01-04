cc873ae0b7bd24b7cb96fa075d7781baf7f2d5a2
Edward Luna <75227309+misterluna@users.noreply.github.com>
2022-01-03 22:43:09 -0800

    [BVC-30] Fix chromatic gatsby link problem

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,26 @@
-const path = require("path");
-const toPath = (_path) => path.join(process.cwd(), _path);
+import { join } from 'path';
+const toPath = (_path) => join(process.cwd(), _path);
 
-module.exports = {
-  "stories": [
-    "../src/templates/*.stories.tsx",
-    "../src/components/*.stories.tsx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-  ],
-  "core": {
-    "builder": "webpack5"
-  },
-  typescript: { reactDocgen: false },
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": toPath("node_modules/@emotion/react"),
-          "emotion-theming": toPath("node_modules/@emotion/react"),
-        },
-      },
-    };
-  },
+export const stories = [
+  '../src/templates/*.stories.tsx',
+  '../src/components/*.stories.tsx',
+  '../src/**/*.stories.@(js|jsx|ts|tsx)',
+];
+export const addons = ['@storybook/addon-links', '@storybook/addon-essentials'];
+export const core = {
+  builder: 'webpack5',
 };
+export const typescript = { reactDocgen: false };
+export async function webpackFinal(config) {
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@emotion/core': toPath('node_modules/@emotion/react'),
+        'emotion-theming': toPath('node_modules/@emotion/react'),
+      },
+    },
+  };
+}
