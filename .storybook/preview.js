98888ecf4d488b167bbf592cff8a3b73bdd55ed4
lucerax <55931447+lucerax@users.noreply.github.com>
2022-01-04 17:50:48 +1100

    [BVC-30] Added Lo-Fi Header with dummy nav links (#4)
    * Added Lo-Fi Header with dummy nav links
    
    * Add ESLint github action and re-run yarn
    
    * Fix CI
    
    * Fix CI part 2
    
    * Fix CI part 3
    
    * Cleanup
    
    * Fix CI 4
    
    * Added Lo-Fi Header with dummy nav links
    
    * [BVC-30] Use Gatsby Link, remove router
    
    * [BVC-30] Fix storybook not working
    
    * CI 5
    
    * remove CRA from storybook config
    
    * Added Lo-Fi Header with dummy nav links
    
    * Add ESLint github action and re-run yarn
    
    * [BVC-30] Regenerate yarn.lock
    
    * Centered header logo and changed color of links
    
    * Centered header logo and changed color of links
    
    * Centered header vertically
    
    * Add ESLint github action and re-run yarn
    
    * [BVC-31] Changed imports in footer file (#5)
    
    * [BVC-31] Changed imports in footer file
    
    * [BVC-31] Use yarn not npm, update gitignore for DS_Store
    
    Co-authored-by: Edward Luna <75227309+misterluna@users.noreply.github.com>
    
    * Remove DS_Store
    
    * Added Lo-Fi Header with dummy nav links
    
    * Add ESLint github action and re-run yarn
    
    * [BVC-30] Use Gatsby Link, remove router
    
    * Added Lo-Fi Header with dummy nav links
    
    * [BVC-30] Rerun yarn
    
    * [BVC-30] Fix chromatic gatsby link problem
    
    * [BVC-30] Fix storybook again
    
    Co-authored-by: Aniruddh Chennapragada <anic@Aniruddhs-MBP.modem>
    Co-authored-by: Edward Luna <75227309+misterluna@users.noreply.github.com>
    Co-authored-by: Rishi Khare <90524732+rishi-khare@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,38 @@
-import { ChakraProvider } from "@chakra-ui/provider"
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { ChakraProvider } from '@chakra-ui/provider';
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+
 import theme from '../src/theme';
 import Fonts from '../src/fonts';
+
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+// This global variable prevents the "__BASE_PATH__ is not defined" error inside Storybook.
+global.__BASE_PATH__ = '/';
+
+window.___navigate = (pathname) => {
+  action('NavigateTo:')(pathname);
+};
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
 const withChakra = (StoryFn) => {
-
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
       <StoryFn />
     </ChakraProvider>
-  )
-}
+  );
+};
 
-export const decorators = [withChakra]
+export const decorators = [withChakra];
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
-
-import { Box, HStack, Text } from '@chakra-ui/react';
+import Link from 'gatsby-link';
+import { Box, HStack, Text, Flex, Heading } from '@chakra-ui/react';
 
 interface HeaderProps {
   pages: string[];
 }
 
-export const Header = ({ pages }: HeaderProps) => (
+const styles = {
+  logo: {
+    fontSize: '30px',
+    lineHeight: '45px',
+    letterSpacing: '0.1em',
+    color: '#636363',
+    textAlign: 'center',
+    display: 'inline-block',
+    position: 'relative',
+    verticalAlign: 'center',
+    width: '100%',
+    height: '100%',
+  },
+};
+
+export const Header: React.FC<HeaderProps> = ({ pages }: HeaderProps) => (
   <Box w="100%" h="70" backgroundColor="#E5E5E5">
-    <HStack>
-      <Text>BVC</Text>
-      {pages[0]}
-    </HStack>
+    <Flex as="nav" alignItems="center" justify="space-between" wrap="wrap" height="100%" color="#636363">
+      <Flex align="center" width="20%" mr={5}>
+        <Heading style={styles.logo}>BVC</Heading>
+      </Flex>
+
+      <HStack spacing={8} marginRight="5%">
+        <Link to="/">
+          <Text>{pages[0]}</Text>
+        </Link>
+        <Link to="/">
+          <Text>{pages[1]}</Text>
+        </Link>
+        <Link to="/">
+          <Text>{pages[2]}</Text>
+        </Link>
+        <Link to="/">
+          <Text>{pages[3]}</Text>
+        </Link>
+        <Link to="/">
+          <Text>{pages[4]}</Text>
+        </Link>
+        <Link to="/">
+          <Text>{pages[5]}</Text>
+        </Link>
+        <Link to="/">
+          <Text>{pages[6]}</Text>
+        </Link>
+      </HStack>
+    </Flex>
   </Box>
 );
