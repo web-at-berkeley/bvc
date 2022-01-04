5f0f0cce6c8404cb3ca8815f18106ac32a681a22
Edward Luna <75227309+misterluna@users.noreply.github.com>
2022-01-03 22:44:52 -0800

    [BVC-30] Fix storybook again

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,29 @@
-import { join } from 'path';
-const toPath = (_path) => join(process.cwd(), _path);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
 
-export const stories = [
-  '../src/templates/*.stories.tsx',
-  '../src/components/*.stories.tsx',
-  '../src/**/*.stories.@(js|jsx|ts|tsx)',
-];
-export const addons = ['@storybook/addon-links', '@storybook/addon-essentials'];
-export const core = {
-  builder: 'webpack5',
-};
-export const typescript = { reactDocgen: false };
-export async function webpackFinal(config) {
-  return {
-    ...config,
-    resolve: {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        '@emotion/core': toPath('node_modules/@emotion/react'),
-        'emotion-theming': toPath('node_modules/@emotion/react'),
+module.exports = {
+  stories: [
+    '../src/templates/*.stories.tsx',
+    '../src/components/*.stories.tsx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5',
+  },
+  typescript: { reactDocgen: false },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
       },
-    },
-  };
-}
+    };
+  },
+};
