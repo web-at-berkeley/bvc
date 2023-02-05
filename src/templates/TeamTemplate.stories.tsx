7a94b367fbfc9dafe539c0ba0d1aaab438cdf12d
yuxinliuhubert <yuxinliuhubert@gmail.com>
2023-02-04 20:42:18 -0800

    Team Live

--- src/templates/TeamTemplate.stories.tsx
+++ src/templates/TeamTemplate.stories.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { TeamTemplate } from './TeamTemplate';
-import placeholder from '../assets/images/teamPlaceholder.png';
+// import placeholder from '../assets/images/teamPlaceholder.png';
 
 export default {
   title: 'Templates/TeamTemplate',
@@ -14,42 +14,42 @@ const Template: ComponentStory<typeof TeamTemplate> = (args) => <TeamTemplate {.
 
 export const Primary = Template.bind({});
 Primary.args = {
-  title: 'Our Team',
-  people: [
-    {
-      photo: placeholder,
-      title: 'President',
-      name: 'Name Name',
-    },
-    {
-      photo: placeholder,
-      title: 'Vice President',
-      name: 'Name Name',
-    },
-    {
-      photo: placeholder,
-      title: 'Director of Curriculum',
-      name: 'Name Name',
-    },
-    {
-      photo: placeholder,
-      title: 'Director of Services',
-      name: 'Name Name',
-    },
-    {
-      photo: placeholder,
-      title: 'Director of Internal',
-      name: 'Name Name',
-    },
-    {
-      photo: placeholder,
-      title: 'Director of Community',
-      name: 'Name Name',
-    },
-  ],
-  bottomText:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. A interdum ac maecenas felis cras odio. Tellus lectus mollis at ultricies velit eu consequat, donec nibh. At quis habitant vel donec purus. Volutpat ac amet, morbi nibh diam tincidunt sed dolor. Convallis aenean et eget sed.',
-  bottomText2: 'Lorem ipsum dolor sit amet, consectetur adipiscing?',
+  // title: 'Our Team',
+  // people: [
+  //   {
+  //     photo: placeholder,
+  //     title: 'President',
+  //     name: 'Name Name',
+  //   },
+  //   {
+  //     photo: placeholder,
+  //     title: 'Vice President',
+  //     name: 'Name Name',
+  //   },
+  //   {
+  //     photo: placeholder,
+  //     title: 'Director of Curriculum',
+  //     name: 'Name Name',
+  //   },
+  //   {
+  //     photo: placeholder,
+  //     title: 'Director of Services',
+  //     name: 'Name Name',
+  //   },
+  //   {
+  //     photo: placeholder,
+  //     title: 'Director of Internal',
+  //     name: 'Name Name',
+  //   },
+  //   {
+  //     photo: placeholder,
+  //     title: 'Director of Community',
+  //     name: 'Name Name',
+  //   },
+  // ],
+  // bottomText:
+  //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. A interdum ac maecenas felis cras odio. Tellus lectus mollis at ultricies velit eu consequat, donec nibh. At quis habitant vel donec purus. Volutpat ac amet, morbi nibh diam tincidunt sed dolor. Convallis aenean et eget sed.',
+  // bottomText2: 'Lorem ipsum dolor sit amet, consectetur adipiscing?',
   navLinks: {
     pages: [
       { linkText: 'Home', linkTarget: '/' },
@@ -61,10 +61,10 @@ Primary.args = {
       { linkText: 'Portal', linkTarget: '/' },
     ],
   },
-  footerLinks: [
-    'https://www.facebook.com/',
-    'https://www.instagram.com/',
-    'https://www.linkedin.com/',
-    'email@example.com',
-  ],
+  // footerLinks: [
+  //   'https://www.facebook.com/',
+  //   'https://www.instagram.com/',
+  //   'https://www.linkedin.com/',
+  //   'email@example.com',
+  // ],
 };
